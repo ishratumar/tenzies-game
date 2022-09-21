2ab8fac18e904121e7c6252193073b3e929b30de
Ishrat <47534248+IshratFatima18@users.noreply.github.com>
2022-09-21 10:41:38 +0500

    dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import Die from "./Components/Die"
 
 export default function App(){
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    const diceElements = dice.map(die => <Die value={die} />)
   return(
-    <main>hellooooo</main>
+    <main>
+      {diceElements}
+    </main>
   )
-}
+  }
--- src/Components/Die.jsx
+++ src/Components/Die.jsx
@@ -1,1 +1,9 @@
 import React from 'react'
+export default function App(props){
+    return(
+      <div className="die-face">
+      <h1 className="die-num">{props.value}</h1>
+      </div>
+    )
+  }
+  
